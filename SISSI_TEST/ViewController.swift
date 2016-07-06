921a7776eefa932f329995ebb81863ec7173f7fc
Sissi Tsai <chinoiseriez@gmail.com>
2016-07-06 19:54:28 +0800

    Initial Commit

---
+++ SISSI_TEST/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SISSI_TEST
+//
+//  Created by Lintinham on 2016/7/6.
+//  Copyright © 2016年 SISSITSAI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
